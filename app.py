18950ac68a1a2f7d27d9fcf268821998b11a83c0
unknown <soyinkas1@gmail.com>
2024-12-05 19:20:15 +0000

    Portfolio Section Started

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, url_for, request
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+from datetime import datetime
+import configparser
+import os
+import re
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'content'
+DIR_BLOG_POSTS = 'blogs'
+DIR_PROJECTS = 'projects'
+
+
+app = Flask(__name__)
+flatpages = FlatPages(app)
+freezer = Freezer(app)
+
+
+
+
+
+
+
+
+
 
 app = Flask(__name__)
 
