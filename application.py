bb878425e9e5b93d64dac6a23f733fb6e83655b7
unknown <soyinkas1@gmail.com>
2024-12-25 13:05:43 +0000

    deploy changes

---
+++ application.py
@@ -0,0 +1,73 @@
+
+from flask import Flask, render_template, url_for, request
+
+# from datetime import datetime
+# import configparser
+import os
+# import re
+import sys
+from app import create_app, db, mail
+from app.db_models import ContactTable
+from flask_migrate import Migrate, init as flask_migrate_init, migrate as flask_migrate_migrate, upgrade as flask_migrate_upgrade
+from dotenv import load_dotenv
+from app.main.exception import CustomException
+from app.main.logging import logging
+
+# Load environment variables
+load_dotenv()
+
+application = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+app = application
+migrate = Migrate(app, db)
+logging.info('app created')
+def make_shell_context():
+    return dict(db=db, Contacts=ContactTable)
+
+app.shell_context_processor(make_shell_context)
+with app.app_context():
+     # Initialize the migration repository if it doesn't exist
+    migrations_path = os.path.join(os.path.dirname(__file__), 'migrations')
+    if not os.path.exists(migrations_path):
+        try:
+            flask_migrate_init()
+            logging.info("Migration repository initialized.")
+        except Exception as e:
+            raise CustomException(e, sys)
+    
+    # Run migrations
+    try:
+        # Generate an initial migration
+        flask_migrate_migrate(message="Initial migration.")
+        # Apply the migration to upgrade the database
+        flask_migrate_upgrade()
+        logging.info('Database upgraded successfuly')
+    except Exception as e:
+            raise CustomException(e, sys)
+
+@app.cli.command()
+def test():
+    """Run the unit tests"""
+    import unittest
+
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
