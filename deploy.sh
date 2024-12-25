ededc3d46f913e019cf2959c1543e16fa8c2dea3
unknown <soyinkas1@gmail.com>
2024-12-25 12:03:34 +0000

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 cd /home/shosowoolu/Web-Profile
-git pull https://github.com/soyinkas1:$GTHUB_TOKEN@github.com/soyinkas1/Web-Profile.git main
+git pull https://github.com/:$GTHUB_TOKEN@github.com/soyinkas1/Web-Profile.git main
 source /home/shosowoolu/.virtualenvs/my-virtualenv/bin/activate
 pip install -r requirements.txt
 # Restart the web app (replace `yourusername` and `yourwebapp` with your actual details)
